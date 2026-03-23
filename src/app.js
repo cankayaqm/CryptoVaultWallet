517d2307424929a60812058af5da6ca54497b493
cankaya <ec24624@qmul.ac.uk>
2026-03-23 20:45:07 +0000

    Fix app configuration

--- src/app.js
+++ src/app.js
@@ -1,3 +1,10 @@
+import express from "express";
 import transactionRoutes from "./routes/transactionRoutes.js";
 
+const app = express();
+
+app.use(express.json());
+
 app.use("/api/transactions", transactionRoutes);
+
+export default app;
